12a77e5517b8c6180b5bd43874b4e6ee09836ce5
priyesh rai <jagritip134@gmail.com>
2026-09-06 12:57:14 +0530

    Enable hierarchical behavior layer

--- launcher.py
+++ launcher.py
@@ -1,15 +1,20 @@
-"""Windows launcher: optimized runtime + cognitive brain + ecosystem + visuals."""
+"""Windows launcher: optimized runtime + cognitive brain + behavior + ecosystem + visuals."""
 from performance_tuning import install as install_performance
 
 install_performance()
 
 from ecosystem_expansion import install as install_ecosystem
 
-install_ecosystem()
+ecosystem_expansion = install_ecosystem
+ecosystem_expansion()
 
 from cognitive_upgrade import install as install_cognition
 
 install_cognition()
+
+from hierarchical_behavior import install as install_behavior
+
+install_behavior()
 
 import main
 
