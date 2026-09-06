2dec5c33d1767016a89d0c6d862808ab51a224cf
priyesh rai <jagritip134@gmail.com>
2026-09-06 13:11:44 +0530

    Wire survival memory and courage into launcher

--- launcher.py
+++ launcher.py
@@ -1,25 +1,23 @@
-"""Windows launcher: optimized runtime + cognitive brain + behavior + ecosystem + visuals."""
+"""Windows launcher: optimized runtime + cognition + behavior + survival memory + visuals."""
 from performance_tuning import install as install_performance
 
 install_performance()
 
 from ecosystem_expansion import install as install_ecosystem
-
-ecosystem_expansion = install_ecosystem
-ecosystem_expansion()
+install_ecosystem()
 
 from cognitive_upgrade import install as install_cognition
-
 install_cognition()
 
 from hierarchical_behavior import install as install_behavior
-
 install_behavior()
+
+from survival_memory_upgrade import install as install_survival_memory
+install_survival_memory()
 
 import main
 
 from visual_upgrade import install as install_visuals
-
 install_visuals()
 
 if __name__ == "__main__":
